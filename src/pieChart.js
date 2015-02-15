08107453864093867c12d2e66735e1142ce93b91
rajushank84 <raju@Natarajans-MacBook-Pro.local>
2015-02-14 17:43:55 -0800

    Adding pie chart logic

--- src/pieChart.js
+++ src/pieChart.js
@@ -6,12 +6,22 @@
 // The PieChart constructor
 var PieChart = function (data, attributes) {
 	this.data = data;
+	this.initColors();
 	this.attributes = attributes;
 	this.draw();
 };
 
 // The PieChart prototype exposing the functionality
 PieChart.prototype = {
+
+	// set colors to data elements if they weren't passed
+	initColors: function () {
+		var colors = ["red", "blue", "green", "orange", "pink", "lightblue", "brown", "lightgreen"];
+		
+		for (var i = 0; i < this.data.length; i++) {
+			this.data[i].color = colors[i];
+		}
+	},
 
 	// draw the pie chart
 	draw: function () {
@@ -24,14 +34,29 @@ PieChart.prototype = {
 		ctx.font = "30px Arial";
 
 		// TODO: Impement
+		var lastend = 0;
+
+		debugger;
+		for (var i = 0; i < this.data.length; i++) {
+			ctx.fillStyle = this.data[i].color;
+			ctx.beginPath();
+
+			ctx.moveTo(200, 150);
+			ctx.arc(200, 150, 150, lastend, lastend + (Math.PI * 2 * (this.data[i].value / 100)), false);
+			
+			ctx.lineTo(200, 150);
+			ctx.fill();
+			
+			lastend += Math.PI * 2 * (this.data[i].value / 100);
+		}
 	},
 
 	// create the canvas DOM object and add it to the body, return it
 	createCanvas: function () {
 		var canvas = document.createElement("canvas"),
 			attributes = this.attributes || {};
-			attributes.width = attributes.width || "960px";
-			attributes.height = attributes.height || "300px";
+			attributes.width = attributes.width || "480";
+			attributes.height = attributes.height || "480px";
 
 		for (attribute in attributes) {
 			canvas.setAttribute(attribute, attributes[attribute]);
